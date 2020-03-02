48e4830800ebc014c3eccc9ceb8c952621feb885
BlackMATov <blackmat@mail.ru>
2020-03-02 21:56:46 +0700

    Happy New Year 🥳

--- untests/catch_main.cpp
+++ untests/catch_main.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of the "https://github.com/blackmatov/ecs.hpp"
  * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2018-2019 Matvey Cherevko
+ * Copyright (C) 2018-2020, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
 #define CATCH_CONFIG_MAIN
